694f0a7dd971ec24059931e007cc49561d5881a6
koleks92 <koleks92@gmail.com>
2023-03-27 21:42:58 +0200

    New page + styling fixes

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("random/", views.random_page, name="random")
+    path("random/", views.random_page, name="random"),
+    path("new_page/", views.new_page, name="new_page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -26,16 +26,19 @@ def sub_check(str):
     
     return results
 
-class SearchWiki(forms.Form):
+class SearchForm(forms.Form):
     search = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Search Enyclopedia'}))
 
+class NewForm(forms.Form):
+    title = forms.CharField(label='',required=True, widget=forms.TextInput(attrs={'placeholder': 'New page title'}))
+    content = forms.CharField(label='',required=True, widget=forms.Textarea(attrs={'placeholder': 'New page content'}))
 
 
 ''' Main functions '''
 def index(request):
 
     return render(request, "encyclopedia/index.html", {
-        "form": SearchWiki(),
+        "form": SearchForm(),
         "entries": util.list_entries()
     })
 
@@ -45,15 +48,18 @@ def entry(request, title):
         html = markdown2.markdown(util.get_entry(title))
         return render(request, "encyclopedia/entry.html", {
             "entry": html,
-            "title": title
+            "title": title,
+            "form": SearchForm()
         })
     else:
-        return render(request, "encyclopedia/notfound.html" , {
-            "title": title.capitalize()
+        return render(request, "encyclopedia/error.html" , {
+            "form": SearchForm(),
+            "title": title.capitalize(),
+            "text": "Requested page was not found or was not added to our database yet."
         })
     
 def search(request):
-    form = SearchWiki(request.GET)
+    form = SearchForm(request.GET)
     if form.is_valid():                         # Check if valid and clean data
         search = form.cleaned_data['search']
         if name_check(search):                  # Check if any form of search is in entries
@@ -62,20 +68,46 @@ def search(request):
         elif sub_check(search):                 # Check if substring of any
             return render(request, 'encyclopedia/search.html', {
                 "search": search,
-                "results" : sub_check(search)
+                "results" : sub_check(search),
+                "form": SearchForm()
             })
         else:                                   # Else render notfound page
             return render(request, "encyclopedia/notfound.html" , {
-            "title": search.capitalize()
+            "title": search.capitalize() + " - Not Found",
+            "form": SearchForm()
             })
     else:
         return render(request, "encyclopedia/layout.html", {
-        "form": SearchWiki(),
+        "form": SearchForm(),
         "entries": util.list_entries()
         })
     
 def random_page(request):
     return entry(request, random.choice(util.list_entries()))
 
+
+def new_page(request):
+    # TODO
+    if request.method == "POST":
+        content = NewForm(request.POST)
+        if content.is_valid():                         # Check if valid and clean data
+            title = content.cleaned_data['title']
+            content = content.cleaned_data['content']
+        if name_check(title):
+            return render(request, "encyclopedia/error.html" , {
+                "form": SearchForm(),
+                "title": "Error",
+                "text": "This title is already in our database."
+            })
+        else:
+            util.save_entry(title, content)
+            
+        print(title)
+        print(content)
+
+    return render(request, "encyclopedia/new_page.html", {
+        "title_form": NewForm(),
+        "form": SearchForm()
+    })
     
 
